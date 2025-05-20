147ce66f64fc52c67db777747852b959d3fe795f
SwapnilPanchade <sspanchade113@gmail.com>
2025-05-20 15:57:19 +0530

    generated the prisma client

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -158,7 +158,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -168,11 +168,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_rmOUF81TzcLE@ep-weathered-voice-a413y5f0-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
